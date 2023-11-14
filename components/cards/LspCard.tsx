042a0b1b95b2cec1b6c4a7a02d2ad5737de4bc51
Dann Wee <dannweezj@gmail.com>
2023-11-15 00:17:56 +0700

    lspcard ui edit

--- components/cards/LspCard.tsx
+++ components/cards/LspCard.tsx
@@ -14,11 +14,11 @@ const LspCard = ({
     return (
         <div className='w-[150px] h-[200px] flex-shrink-0 shadow-md mb-5 rounded-3xl bg-white hover:scale-110 transition-all mt-[20px] cursor-pointer flex flex-col text-black bg-[#FDDCE8]/80 p-3 hover:bg-[#85A0FF]/70 hover:text-white'>
             <div className='flex items-center justify-center'>
-                <Image src={`https://universalpage.dev/api/ipfs/${images}`} alt='NFT' width={110} height={110} className='nftImage rounded-lg' />
+                <Image src={`https://universalpage.dev/api/ipfs/${images}`} alt='NFT' width={60} height={60} className='nftImage rounded-lg' />
             </div>
-            <div className="h-2"></div>
+            <div className="h-4"></div>
             <Separator />
-            <div className="h-2"></div>
+            <div className="h-4"></div>
             <div className='text-xs font-semibold'>{description}</div>
             <div className="h-2"></div>
         </div>
