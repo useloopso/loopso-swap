8c3f57e4948332eb3cc35ed5b50d1b9a5f8e25c3
thomas779 <thomasehumphreys@gmail.com>
2023-11-27 02:03:03 -0300

    add destination token

--- components/shared/SwapWidget.tsx
+++ components/shared/SwapWidget.tsx
@@ -3,8 +3,23 @@
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { fadeIn, staggerContainer } from "@/utils/motion";
-import { BadgeInfo, InfinityIcon, MoveDown, Repeat2 } from "lucide-react";
-import { ERC20_ABI, bridgeTokens, checkTokenAllowance, getAttestationIDHash, getContractAddressFromChainId, getLoopsoContractFromContractAddr, getWrappedTokenInfo, wrapNativeToken } from "loopso-bridge-sdk";
+import {
+  BadgeInfo,
+  InfinityIcon,
+  MoveDown,
+  Repeat2,
+  BookUser,
+} from "lucide-react";
+import {
+  ERC20_ABI,
+  bridgeTokens,
+  checkTokenAllowance,
+  getAttestationIDHash,
+  getContractAddressFromChainId,
+  getLoopsoContractFromContractAddr,
+  getWrappedTokenInfo,
+  wrapNativeToken,
+} from "loopso-bridge-sdk";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import SelectTokenModal from "../modal/SelectTokenModal";
@@ -18,21 +33,23 @@ import SelectSourceChainModal from "../modal/SelectSourceChainModal";
 import SelectDestinationChainModal from "../modal/SelectDestinationChainModal";
 import { ERC20Token, NativeToken, Network, Token } from "@/lib/types";
 import { useConnectWallet, useWallets } from "@web3-onboard/react";
-import {  TransactionResponse, ethers } from "ethers";
+import { TransactionResponse, ethers } from "ethers";
 import { useWrappedTokensReleased } from "@/hooks/useWrappedTokensReleased";
 import { useTokensReleased } from "@/hooks/useTokensReleased";
 import { useSameNetwork } from "@/hooks/useSameNetwork";
-import { toast } from 'sonner'
+import { toast } from "sonner";
 import { getExplorerTransaction } from "@/helpers/getExplorerTransaction";
 import SelectTokenModalV2 from "../modal/SelectTokenModalV2";
 
-
-
-
 const SwapWidget = () => {
-  const [selectedSourceChainNetwork, setSelectedSourceChainNetwork] = useState<Network | undefined>(undefined);
-  const [selectedDestinationChainNetwork, setSelectedDestinationChainNetwork] = useState<Network | undefined>(undefined);
-  const [selectedSourceToken, setSelectedSourceToken] = useState<ERC20Token | NativeToken | undefined>(undefined);
+  const [selectedSourceChainNetwork, setSelectedSourceChainNetwork] = useState<
+    Network | undefined
+  >(undefined);
+  const [selectedDestinationChainNetwork, setSelectedDestinationChainNetwork] =
+    useState<Network | undefined>(undefined);
+  const [selectedSourceToken, setSelectedSourceToken] = useState<
+    ERC20Token | NativeToken | undefined
+  >(undefined);
 
   const [amount, setAmount] = useState<string>("");
   const [txHash, setTxHash] = useState<string>("");
@@ -51,59 +68,94 @@ const SwapWidget = () => {
     selectedDestinationChainNetwork?.chainId
   );
 
+  const [isInputVisible, setIsInputVisible] = useState(false);
+  const [selectedDstAddress, setDstAddress] = useState("");
+
+  const toggleSwitch = () => {
+    setIsInputVisible(!isInputVisible);
+  };
+
+  const handleAddressChange = (e: any) => {
+    // @todo check valid hex address
+    setDstAddress(e.target.value);
+  };
+
   const handleSameNetwork = () => {
     window.alert("Source and Destination networks should not be the same.");
     setSelectedDestinationChainNetwork(undefined);
   };
 
-  useSameNetwork(selectedSourceChainNetwork, selectedDestinationChainNetwork, handleSameNetwork);
+  useSameNetwork(
+    selectedSourceChainNetwork,
+    selectedDestinationChainNetwork,
+    handleSameNetwork
+  );
 
   const openNewTab = (url: any) => {
-    window.open(url, '_blank');
+    window.open(url, "_blank");
   };
 
-  useEffect(() => {
-
-  }, [txHash, wrappedTokensReleased, tokensReleased]);
+  useEffect(() => {}, [txHash, wrappedTokensReleased, tokensReleased]);
 
   const handleSubmitAndBridge = async () => {
-    if (!wallet || !selectedSourceToken || !selectedSourceChainNetwork || !selectedDestinationChainNetwork) {
+    if (
+      !wallet ||
+      !selectedSourceToken ||
+      !selectedSourceChainNetwork ||
+      !selectedDestinationChainNetwork
+    ) {
       // Handle missing values or conditions
       return;
     }
-  
+
     let isOnLukso = connectedWallet?.label === "Universal Profiles";
-    const ethersProvider = new ethers.BrowserProvider(isOnLukso ? window.lukso : wallet.provider);
+    const ethersProvider = new ethers.BrowserProvider(
+      isOnLukso ? window.lukso : wallet.provider
+    );
     const signer = await ethersProvider.getSigner();
-  
-    let _txHash: TransactionResponse | null = null; 
-	let convertedAmount = BigInt(Number(amount) * (10 ** 18))
-  
+
+    let _txHash: TransactionResponse | null = null;
+    let convertedAmount = BigInt(Number(amount) * 10 ** 18);
+
     try {
       if (selectedSourceToken.isNative) {
-        const wrappedTx = await wrapNativeToken(signer, selectedSourceChainNetwork.chainId, convertedAmount);
+        const wrappedTx = await wrapNativeToken(
+          signer,
+          selectedSourceChainNetwork.chainId,
+          convertedAmount
+        );
         if (!wrappedTx) {
           throw new Error("Failed to wrap native token");
         }
       }
-  
+
       _txHash = await bridgeTokens(
         selectedSourceChainNetwork.loopsoContractAddress,
         signer,
         selectedSourceToken.token_address,
         convertedAmount,
-        wallet?.accounts[0].address,
+        selectedDstAddress || wallet?.accounts[0].address,
         selectedDestinationChainNetwork.chainId
       );
     } catch (error) {
       console.error("Error bridging tokens:", error);
     }
-  
+
     if (_txHash) {
       setTxHash(_txHash.hash);
       toast.info(
-        <div onClick={() => openNewTab(getExplorerTransaction(selectedSourceChainNetwork.chainId, _txHash!.hash ))} className='cursor-pointer'>
-          <span className='font-semibold'>🍻 Transaction Created 🍻</span>
+        <div
+          onClick={() =>
+            openNewTab(
+              getExplorerTransaction(
+                selectedSourceChainNetwork.chainId,
+                _txHash!.hash
+              )
+            )
+          }
+          className="cursor-pointer"
+        >
+          <span className="font-semibold">🍻 Transaction Created 🍻</span>
           <br />
           Click here to view your transaction.
         </div>,
@@ -111,12 +163,9 @@ const SwapWidget = () => {
       );
     } else {
       setTxHash("ERROR: No tx hash");
-      toast.error(
-        <div>
-          🛑 Something went wrong. Please try again.
-        </div>,
-        { duration: 8000 }
-      );
+      toast.error(<div>🛑 Something went wrong. Please try again.</div>, {
+        duration: 8000,
+      });
     }
   };
 
@@ -155,7 +204,7 @@ const SwapWidget = () => {
               type="number"
               value={amount}
               onChange={(e) => {
-                const inputValue = e.target.value.replace(/[^0-9.]/g, '');
+                const inputValue = e.target.value.replace(/[^0-9.]/g, "");
                 if (
                   selectedSourceChainNetwork &&
                   selectedSourceToken &&
@@ -170,13 +219,13 @@ const SwapWidget = () => {
                 !selectedDestinationChainNetwork
               }
             />
-            <div className={selectedSourceChainNetwork ? '' : 'disabled-modal'}>
+            <div className={selectedSourceChainNetwork ? "" : "disabled-modal"}>
               {/* <SelectTokenModal
                 network={selectedSourceChainNetwork}
                 selectedToken={selectedSourceToken}
                 setSelectedToken={setSelectedSourceToken}
               /> */}
-              <SelectTokenModalV2 
+              <SelectTokenModalV2
                 network={selectedSourceChainNetwork}
                 selectedToken={selectedSourceToken}
                 setSelectedToken={setSelectedSourceToken}
@@ -192,7 +241,13 @@ const SwapWidget = () => {
         <div className="flex items-center gap-6 pl-2">
           <InfinityIcon />
           <p className="font-semibold text-sm">To</p>
-          <div className={selectedSourceChainNetwork && selectedSourceToken ? '' : 'disabled-modal'}>
+          <div
+            className={
+              selectedSourceChainNetwork && selectedSourceToken
+                ? ""
+                : "disabled-modal"
+            }
+          >
             <SelectDestinationChainModal
               setSelectedNetwork={setSelectedDestinationChainNetwork}
               selectedNetwork={selectedDestinationChainNetwork}
@@ -209,7 +264,8 @@ const SwapWidget = () => {
                 </TooltipTrigger>
                 <TooltipContent>
                   <p>
-                    This amount is estimated based on the current bridge rate and fees.
+                    This amount is estimated based on the current bridge rate
+                    and fees.
                   </p>
                 </TooltipContent>
               </Tooltip>
@@ -223,13 +279,31 @@ const SwapWidget = () => {
               type="number"
               disabled={true}
             />
+            <BookUser onClick={toggleSwitch} />
           </div>
-          {amount ? 
-          <p className="text-xs pt-2 ml-1">
-            <span className='font-semibold'>Bridge Fee:&nbsp;</span>
-            {fee}
-          </p>
-          : null }
+          {amount ? (
+            <p className="text-xs pt-2 ml-1">
+              <span className="font-semibold">Bridge Fee:&nbsp;</span>
+              {fee}
+            </p>
+          ) : null}
+        </div>
+        <div>
+          {isInputVisible && (
+            <div>
+              <label>
+                <div className="flex items-center justify-center p-1 mt-4">
+                  <p className="text-sm font-bold w-56">Destination address:</p>
+                  <Input
+                    className="bg-[#E1E1FF] rounded-3xl p-2 text-sm"
+                    value={selectedDstAddress}
+                    onChange={handleAddressChange}
+                    type="string"
+                  />
+                </div>
+              </label>
+            </div>
+          )}
         </div>
         <div className="h-4"></div>
         <div className="items-center justify-center flex">
@@ -247,7 +321,7 @@ const SwapWidget = () => {
             Swap
           </Button>
         </div>
-    </motion.div>
+      </motion.div>
     </motion.div>
   );
 };
